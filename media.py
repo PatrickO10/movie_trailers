74e3f47f23cf5866aea107d798fe74865abad73f
patricko10 <patrickorth10@gmail.com>
2015-05-20 19:26:04 -0500

    adds comments and changes indentation for movie objects.

--- media.py
+++ media.py
@@ -1,10 +1,7 @@
 import webbrowser
 
-# Comment the class itself. This is class about movies.
 class Movie():
-    '''
-    This is a class about movies.
-    '''
+    '''This is a class for storing information about movies.'''
     def __init__(self, movie_title, movie_year, poster_image, trailer_youtube, movie_rating):
         self.title = movie_title
         self.year = movie_year
@@ -12,7 +9,5 @@ class Movie():
         self.trailer_youtube_url = trailer_youtube
         self.rating = movie_rating
     def show_trailer(self):
-        '''
-        This method opens a youtube url.
-        '''
+        '''This method opens a youtube url.'''
         webbrowser.open(self.trailer_youtube_url)
